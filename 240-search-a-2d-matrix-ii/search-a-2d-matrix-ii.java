bed11bf48fb78e29b5b61cc8743e0948c7981ffe
Kartik Borse <kartikborse056@gmail.com>
2025-06-30 22:13:32 +0530

    Time: 6 ms (47.58%) | Memory: 46 MB (57.66%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -1,27 +1,48 @@
+// class Solution {
+//     public boolean searchMatrix(int matrix[][], int target) {
+        
+//        for(int i=0;i<matrix.length;i++)
+//        {
+//         int st=0;
+//         int end=matrix[0].length-1;
+//         while(st<=end)
+//         {
+//         int mid=(st+end)/2;
+//         if(matrix[i][mid]==target)
+//         {
+//             return true;
+//         }
+//         else if(matrix[i][mid]<target)
+//         {
+//             st=mid+1;
+//         }
+//         else
+//         {
+//             end=mid-1;
+//         }
+//        }
+//        }
+//        return false;
+//     }
+// }
 class Solution {
     public boolean searchMatrix(int matrix[][], int target) {
-        
-       for(int i=0;i<matrix.length;i++)
-       {
-        int st=0;
-        int end=matrix[0].length-1;
-        while(st<=end)
+        int row=0;
+        int col=matrix[0].length-1;
+        while(row<=matrix.length-1 && col>=0)
         {
-        int mid=(st+end)/2;
-        if(matrix[i][mid]==target)
+        if(target==matrix[row][col])
         {
             return true;
         }
-        else if(matrix[i][mid]<target)
+        else if(target < matrix[row][col])
         {
-            st=mid+1;
+            col--;
         }
-        else
-        {
-            end=mid-1;
+        else{
+            row++;
         }
-       }
-       }
-       return false;
+        }
+        return false;
     }
 }
