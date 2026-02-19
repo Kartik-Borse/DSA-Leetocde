aac0547690608be190b07334756a4723a4d95773
Kartik Borse <kartikborse056@gmail.com>
2026-02-19 23:25:16 +0530

    Time: 0 ms (100%), Space: 48.3 MB (85.31%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int st=0,end=nums.length-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(target < nums[mid]){
+                end=mid-1;
+            }
+            else if(target > nums[mid]){
+                st=mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+        
+    }
+}
